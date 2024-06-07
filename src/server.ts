3d120eff4eb6c4532aac2c945506e33bfa107952
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-07 12:03:28 -0300

    wip: in progress

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,43 @@
 import express from 'express';
-import { getPayload } from './get-payload';
+import { getPayload } from 'payload';
+import { getPayloadClient } from './get-payload';
+import { nextApp, nextHandler } from './next-utils'
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 
 const start = async () => {
-  const payload = await getPayload();
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
+      },
+    },
+  });
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        'Next.js is building for production'
+      )
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
+    })
+
+    return
+  }
+  app.use((req, res) => nextHandler(req, res))
+  nextApp.prepare().then( () =>{
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      )
+    })
+  })
 }
 
 start();  
