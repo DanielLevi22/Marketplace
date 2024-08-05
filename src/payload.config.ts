a6af852450d0a36e763259e0b6fe3fc174c01e15
Daniel Levi <daniellevi000@gmail.com>
2024-08-05 15:13:16 -0300

    wip: create collection products

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,8 @@ import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
 import { Users } from './collections/users'
 import dotenv from 'dotenv'
+import { Products } from './collections/products/products'
+
 // import { Products } from './collections/Products/Products'
 // import { Media } from './collections/Media'
 // import { ProductFiles } from './collections/ProductFile'
@@ -17,7 +19,7 @@ dotenv.config({
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
   // collections: [Users, Products, Media, ProductFiles, Orders],
-  collections: [Users],
+  collections: [Users, Products],
   routes: {
     admin: '/sell',
   },
