595616a015f0eee151df1a66e0c1e04c5276848c
Daniel Levi <daniellevi000@gmail.com>
2024-08-06 13:31:53 -0300

    feat: create collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,9 @@ import path from 'path'
 import { Users } from './collections/users'
 import dotenv from 'dotenv'
 import { Products } from './collections/products/products'
+import { Media } from './collections/media'
+import { ProductFiles } from './collections/product-file'
+import { Orders } from './collections/orders'
 
 // import { Products } from './collections/Products/Products'
 // import { Media } from './collections/Media'
@@ -18,8 +21,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  // collections: [Users, Products, Media, ProductFiles, Orders],
-  collections: [Users, Products],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: '/sell',
   },
